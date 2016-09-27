2982ca21e36b37f63e65ffe7d0a86a78c70d2b35
Tyler Cai <torkyguy@gmail.com>
2016-09-26 20:08:36 -0400

    Blue Box appears bro
    Box select blue box is here boys.

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 
 public class CameraControl : MonoBehaviour
 {
 
     public GameObject rightClickCursor;
     public GameObject leftClickCursor;
+    public Canvas canvas;
     public Cursor rightCursor;
     public Cursor leftCursor;
     Ray start;
     Ray end;
+    float startX;
+    float startY;
+    float endX;
+    float endY;
+    bool drawingBox = false;
 
     public List<GameObject> selected = new List<GameObject>();
 
@@ -30,6 +37,7 @@ public class CameraControl : MonoBehaviour
         CameraX = gameObject.transform.position.x;
         CameraY = gameObject.transform.position.y;
         CameraZ = gameObject.transform.position.z;
+        drawingBox = false;
     }
 
     // Update is called once per frame
@@ -60,19 +68,20 @@ public class CameraControl : MonoBehaviour
             CameraY += CameraSpeed;
         }
 
-        if (rightCursor.Select(KeyCode.Mouse1) && selected.Count>0)
+        if (rightCursor.Select(KeyCode.Mouse1) && selected.Count > 0)
         {
             GameObject rightSelected = rightCursor.GetSelected();
             //Check for team tag later. This is techdebt that needs to be added later on.
-            if(rightSelected!=null && rightSelected.CompareTag("Unit"))
+            if (rightSelected != null && rightSelected.CompareTag("Unit"))
             {
-                foreach(GameObject selectedObject in selected)
+                foreach (GameObject selectedObject in selected)
                 {
                     Unit selectedUnit = selectedObject.GetComponent(typeof(Unit)) as Unit;
                     selectedUnit.target = rightSelected;
                     selectedUnit.state = (int)Unit.Mode.MoveToTarget;
                 }
-            }else
+            }
+            else
             {
                 foreach (GameObject selectedObject in selected)
                 {
@@ -92,6 +101,15 @@ public class CameraControl : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             start = Camera.main.ScreenPointToRay(Input.mousePosition);
+            startX = Input.mousePosition.x;
+            startY = Input.mousePosition.y;
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            endX = Input.mousePosition.x;
+            endY = Input.mousePosition.y;
+            drawingBox = true;
         }
 
         if (Input.GetKeyUp(KeyCode.Mouse0))
@@ -101,6 +119,7 @@ public class CameraControl : MonoBehaviour
             {
                 selected = BoxSelect.GetSelected(start, end);
             }
+            drawingBox = false;
         }
 
         if (leftCursor.Select(KeyCode.Mouse0))
@@ -112,5 +131,55 @@ public class CameraControl : MonoBehaviour
             }
             Debug.Log(objectSelected);
         }
+    }
+
+    void OnGUI()
+    {
+        if (drawingBox)
+        {
+            drawRect(startX, startY, endX, endY);
+        }
+    }
+
+    void drawRect(float x, float y, float x1, float y1)
+    {
+        float width = x1 - x;
+        float height = y1 - y;
+
+        var topX = x;
+        var topY = y;
+
+        if (width < 0)
+        {
+            width *= -1;
+            topX = x-width;
+        }
+
+        if (height < 0)
+        {
+            height *= -1;
+        }else
+        {
+            topY = y + height;
+        }
+
+        Debug.Log("x: " + x + ", y: " + y + ", width: " + width + ", height: " + height);
+        Rect r = new Rect(topX, Screen.height - topY, width, height);
+        //GUIStyle guiStyle = new GUIStyle();
+        //if (width > 10 && height > 10)
+        //{
+        //    GUI.Label(r, "");
+        //}
+        DrawQuad(r, new Color(0, 0, 255, .5F));
+
+    }
+
+    void DrawQuad(Rect position, Color color)
+    {
+        Texture2D texture = new Texture2D(1, 1);
+        texture.SetPixel(0, 0, color);
+        texture.Apply();
+        GUI.skin.box.normal.background = texture;
+        GUI.Box(position, GUIContent.none);
     }
 }
